8a75ace54e5a2499b5d70d4b488793d4468a986a
Stanislav Petryakov <become.glory@yandex.ru>
2014-04-04 14:48:22 +0400

    Finish static

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Profit Sync'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Profit Sync')
+    end
+	
+	it "should have the title 'Profit Sync - Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Profit Sync - Home')
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'About Profit Sync'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Profit Sync')
+    end
+	
+	it "should have the title 'Profit Sync - About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Profit Sync - About')
+    end
+  end
+end
