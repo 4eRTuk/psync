0a56e25d4f9c923a7fe216074c1c31f85a3363ac
Stanislav Petryakov <become.glory@yandex.ru>
2014-04-04 13:00:42 +0400

    Initial

---
+++ secret_token — копия.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Psync::Application.config.secret_key_base = '66ec9e3e4c755845c3bc0ee7c458790940093bd6bdfc402e16bb70c6ff810f7d8f3cedc0ab802631ac6805f57f4b09c121ae056e324432cad6150af8500eda83'
