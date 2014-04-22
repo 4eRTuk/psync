8fb5c27e257edebc2a8a3fcdc92aa7b1f6041f83
Stanislav Petryakov <become.glory@yandex.ru>
2014-04-22 20:58:55 +0400

    Finish buglist on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
 	@posts = Vpost.limit(5)
 	@total = Vpost.count
+	@bugs = open("http://dl.dropboxusercontent.com/s/509hk5wg3fjgfc4/buglist.txt").readlines
   end
   
   def about
